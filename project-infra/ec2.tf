dd6273e5a4cc0d6fe01e8e799f043689af09099a
akashjaiswal36 <akash.jaiswal36@gmail.com>
2025-08-07 04:05:08 +0530

    Initial commit

---
+++ project-infra/ec2.tf
@@ -0,0 +1,15 @@
+resource "aws_launch_template" "web_template" {
+  name_prefix   = "web-server"
+  image_id      = "ami-020cba7c55df1f615"
+  instance_type = "t2.micro"
+
+  user_data = base64encode(templatefile("${path.module}/userdata.tpl", {
+    ansible_pull_url = "https://github.com/akashjaiswal36/aws-ec2-lb-terraform-ansible-project.git"
+  }))
+
+  iam_instance_profile {
+    name = aws_iam_instance_profile.web_profile.name
+  }
+
+  security_group_names = ["web-sg"]
+}
--- project-infra/main.tf
+++ project-infra/main.tf
@@ -1,3 +1,43 @@
 provider "aws" {
   region = "ap-south-1"
 }
+
+resource "aws_instance" "web" {
+  ami           = "ami-020cba7c55df1f615" # Ubuntu AMI
+  instance_type = "t2.micro"
+  key_name      = "aws-key.pem"
+
+  user_data = <<-EOF
+              #!/bin/bash
+              apt update && apt install -y ansible git
+              ansible-pull -U https://github.com/akashjaiswal36/aws-ec2-lb-terraform-ansible-project.git ansible/playbooks/webserver.yml
+              EOF
+
+  vpc_security_group_ids = [aws_security_group.web_sg.id]
+}
+
+resource "aws_security_group" "web_sg" {
+  name        = "web_sg"
+  description = "Allow SSH and HTTP"
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
